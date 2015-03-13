df14eb8d85dd1f4e37ef0f5deff93f7af9c13764
Remi Palandri <rpalandri@apple.com>
2015-03-12 21:22:30 -0700

    added first source file, better gitignore and Xcode project

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lvr
+//
+//  Created by Remi Palandri on 12/03/15.
+//  Copyright (c) 2015 tuxer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
